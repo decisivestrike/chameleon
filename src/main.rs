2560fb6366a3fe15e5c8b605e9e245f3bbe633c6
Denis <zak63507@gmail.com>
2025-10-30 14:54:41 +0300

    add widget layer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,75 @@
-fn main() {
-    println!("Hello, world!");
+mod widgets;
+
+use grapes::gtk;
+use gtk::glib::{self};
+use gtk::prelude::*;
+
+use crate::widgets::WidgetLayer;
+
+fn build_ui(application: &gtk::Application) {
+    let widget_layer = WidgetLayer::new(application);
+
+    {
+        let l = gtk::Label::new(Some("Drag Me!"));
+        widget_layer.append(&l, 50.0, 50.0);
+    }
+
+    {
+        let l = gtk::Label::new(Some("Drag Me Too!"));
+        widget_layer.append(&l, 100.0, 100.0);
+    }
+
+    {
+        let l = gtk::Label::new(Some("Pretty good!"));
+        widget_layer.append(&l, 150.0, 150.0);
+    }
+
+    widget_layer.present();
+}
+
+fn main() -> glib::ExitCode {
+    let app = gtk::Application::builder()
+        .application_id("decisivestrike.chameleon")
+        .build();
+
+    app.connect_startup(|_| {
+        let provider = gtk::CssProvider::new();
+        provider.load_from_string(
+            r#"
+            #widget-layer {
+                margin: 0px;
+                padding: 0px;
+                border: none;
+                background-color: rgba(0, 0, 0, 0.0);
+            }
+            
+            box {
+                margin: 0px;
+                padding: 0px;
+                border: none;
+                border-radius: 0px;
+            }
+
+            label {
+                font-family: "JetBrainMono Nerd Font Mono";
+                color: whitesmoke;
+                font-size: 40px;
+                padding: 4px 10px;
+                text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.7);
+                border: 2px dashed white;
+                border-radius: 10px;
+            }
+            "#,
+        );
+
+        gtk::style_context_add_provider_for_display(
+            &gtk::gdk::Display::default().expect("Could not connect to a display."),
+            &provider,
+            gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+        );
+    });
+
+    app.connect_activate(|app| build_ui(app));
+
+    app.run()
 }
---
+++ src/widgets/mod.rs
@@ -0,0 +1,180 @@
+use grapes::{
+    gtk::{
+        self, ApplicationWindow, EventControllerKey, Fixed, Orientation, Widget,
+        gdk::Key,
+        glib::clone,
+        prelude::{GtkWindowExt, *},
+    },
+    layer_shell,
+};
+use layer_shell::{KeyboardMode, Layer, LayerShell};
+use std::{
+    cell::{Cell, RefCell},
+    rc::Rc,
+};
+
+#[derive(Clone)]
+pub struct WidgetLayer {
+    window: ApplicationWindow,
+    fixer: Fixed,
+    active_widget: Rc<RefCell<Option<Widget>>>,
+    tracked_widget: Rc<RefCell<Option<Widget>>>,
+    previous_mouse_positon: Rc<Cell<(f64, f64)>>,
+}
+
+impl WidgetLayer {
+    pub fn new(application: &gtk::Application) -> Self {
+        let window = ApplicationWindow::new(application);
+
+        let fixer = Fixed::new();
+        window.set_child(Some(&fixer));
+
+        let layer = Self {
+            window,
+            fixer,
+            active_widget: Default::default(),
+            tracked_widget: Default::default(),
+            previous_mouse_positon: Default::default(),
+        };
+
+        layer.setup();
+
+        layer
+    }
+
+    pub fn append(&self, widget: &impl IsA<Widget>, x: f64, y: f64) {
+        let wrapper = gtk::Box::new(Orientation::Horizontal, 0);
+        let motion_controller = gtk::EventControllerMotion::new();
+
+        motion_controller.connect_leave(clone!(
+            #[strong(rename_to=active_widget)]
+            self.active_widget,
+            move |_| *active_widget.borrow_mut() = None
+        ));
+
+        motion_controller.connect_enter(clone!(
+            #[strong(rename_to=active_widget)]
+            self.active_widget,
+            #[strong]
+            wrapper,
+            move |_, _, _| *active_widget.borrow_mut() = Some(wrapper.clone().into())
+        ));
+
+        wrapper.add_controller(motion_controller);
+        wrapper.append(widget);
+
+        self.fixer.put(&wrapper, x, y);
+    }
+
+    /// This is almost an ordinary window, so it should be presented.
+    pub fn present(&self) {
+        self.window.present();
+    }
+
+    fn setup(&self) {
+        // Screen W H
+        const WIDTH: i32 = 1920;
+        const HEIGHT: i32 = 1080;
+
+        let window = &self.window;
+
+        window.set_widget_name("widget-layer");
+        window.set_default_size(WIDTH, HEIGHT);
+
+        window.set_decorated(false);
+        window.set_resizable(false);
+
+        window.set_hexpand(false);
+        window.set_vexpand(false);
+
+        window.init_layer_shell();
+        window.set_keyboard_mode(KeyboardMode::OnDemand);
+        window.set_layer(Layer::Bottom);
+
+        self.setup_motion_controller();
+        self.setup_tracker();
+    }
+
+    /// Allows to track the widget
+    fn setup_tracker(&self) {
+        let key_controller = EventControllerKey::new();
+
+        key_controller.connect_key_pressed(clone!(
+            #[strong(rename_to=tracked)]
+            self.tracked_widget,
+            #[strong(rename_to=active)]
+            self.active_widget,
+            move |_, key, _keyval, _state| {
+                if key == Key::Super_L {
+                    *tracked.borrow_mut() = active.borrow().clone();
+                }
+                gtk::glib::Propagation::Proceed
+            }
+        ));
+
+        key_controller.connect_key_released(clone!(
+            #[strong(rename_to=tracked)]
+            self.tracked_widget,
+            move |_, key, _keyval, _state| {
+                if key == Key::Super_L {
+                    *tracked.borrow_mut() = None;
+                }
+            }
+        ));
+
+        self.window.add_controller(key_controller);
+    }
+
+    fn setup_motion_controller(&self) {
+        let motion_controller = gtk::EventControllerMotion::new();
+
+        motion_controller.connect_motion(clone!(
+            #[strong(rename_to=prev)]
+            self.previous_mouse_positon,
+            #[strong(rename_to=tracked_widget)]
+            self.tracked_widget,
+            #[strong(rename_to=fixer)]
+            self.fixer,
+            move |_, x, y| {
+                let tracked = tracked_widget.borrow();
+
+                if let Some(widget) = tracked.as_ref() {
+                    let (prev_x, prev_y) = prev.get();
+                    let (wx, wy) = fixer.child_position(widget);
+
+                    let diff_x = x - prev_x;
+                    let diff_y = y - prev_y;
+
+                    let max_x = (1920 - widget.width()) as f64;
+                    let new_x_unchecked = wx + diff_x;
+
+                    let new_x = if new_x_unchecked > max_x {
+                        max_x
+                    } else if new_x_unchecked < 0.0 {
+                        0.0
+                    } else {
+                        new_x_unchecked
+                    };
+
+                    let max_y = (1080 - widget.height()) as f64;
+                    let new_y_unchecked = wy + diff_y;
+
+                    let new_y = if new_y_unchecked > max_y {
+                        max_y
+                    } else if new_y_unchecked < 0.0 {
+                        0.0
+                    } else {
+                        new_y_unchecked
+                    };
+
+                    println!("Move to {new_x} {new_y}");
+                    fixer.move_(widget, new_x, new_y);
+                }
+
+                prev.set((x, y));
+            }
+        ));
+
+        self.window.add_controller(motion_controller);
+    }
+}
