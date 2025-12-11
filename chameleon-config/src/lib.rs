a57a7d78539f3d34fcb75f207acf1c0fe1fa0add
inqlog <not@example.com>
2025-12-11 21:19:47 +0300

    redo the config; add anyhow

--- chameleon-config/src/lib.rs
+++ chameleon-config/src/lib.rs
@@ -2,84 +2,64 @@ pub mod bar;
 pub use bar::Bar;
 
 pub mod widgets;
-use grapes::tokio::sync::RwLock;
 pub use widgets::Widgets;
 
+use anyhow::{Result, bail};
+use grapes::tokio::sync::broadcast::{self, Receiver, Sender};
 use log::error;
 use serde::Deserialize;
 use std::{
     fmt,
     path::{Path, PathBuf},
+    sync::{Arc, LazyLock, OnceLock},
 };
 
-pub static CONFIG: RwLock<Option<Config>> = RwLock::const_new(None);
+static CONFIG_PATH: OnceLock<PathBuf> = OnceLock::new();
+static TX: LazyLock<Sender<Arc<Config>>> =
+    LazyLock::new(|| broadcast::channel(64).0);
 
 #[derive(Debug, Deserialize)]
-pub struct ConfigInner {
+pub struct Config {
     #[serde(default, rename = "widgets")]
     pub widgets: Widgets,
-    #[serde(default, rename = "taskbar")]
+    #[serde(default, rename = "taskbar")] // Statusbar panel
     pub bar: Bar,
 }
 
-#[derive(Debug)]
-pub struct Config {
-    path: PathBuf,
-    inner: ConfigInner,
-}
-
 impl Config {
-    fn new(path: impl AsRef<Path>, inner: ConfigInner) -> Self {
-        Self {
-            path: path.as_ref().to_path_buf(),
-            inner,
-        }
-    }
-
     pub fn init<P>(path: P)
     where
         P: AsRef<Path> + fmt::Debug,
     {
+        CONFIG_PATH.get_or_init(|| path.as_ref().to_path_buf());
+
+        if let Err(e) = Self::update() {
+            error!("{e}");
+            std::process::exit(-1);
+        }
+    }
+
+    pub fn subscribe() -> Receiver<Arc<Config>> {
+        TX.subscribe()
+    }
+
+    fn update() -> Result<()> {
+        let path = CONFIG_PATH.get().unwrap();
+
         let toml_str = match std::fs::read_to_string(&path) {
             Ok(file) => file,
             Err(e) => {
-                error!("Failed to open config file at {path:?}. {e}");
-                std::process::exit(-1);
+                bail!("Failed to open config file at {path:?}. {e}")
             }
         };
 
         match toml::from_str(&toml_str) {
-            Ok(inner) => {
-                *CONFIG.blocking_write() = Some(Config::new(path, inner))
+            Ok(config) => {
+                TX.send(Arc::new(config)).unwrap();
+                Ok(())
             }
-            Err(e) => {
-                error!("{e}");
-                std::process::exit(-1);
-            }
-        };
-    }
 
-    fn update(&self) {
-        let toml_str = match std::fs::read_to_string(&self.path) {
-            Ok(file) => file,
-            Err(e) => {
-                error!("Failed to open config file at {:?}. {e}", self.path);
-                return;
-            }
-        };
-
-        match toml::from_str(&toml_str) {
-            Ok(config) => *CONFIG.blocking_write() = config,
-            Err(e) => {
-                error!("{e}");
-                std::process::exit(-1);
-            }
-        };
-
-        // broadcast
-    }
-
-    pub fn read(config: &'static RwLock<Option<Self>>) -> &'static ConfigInner {
-        &config.inner
+            Err(e) => bail!("{e}"),
+        }
     }
 }
