47f39993576bc255715c92e7749a7ed08f0c739b
Denis <zak63507@gmail.com>
2026-03-03 15:07:02 +0300

    some changes in launcher window

--- chameleon-launcher/src/lib.rs
+++ chameleon-launcher/src/lib.rs
@@ -1,24 +1,90 @@
-use grapes::glib::{self, clone};
-use grapes::gtk::EventControllerKey;
+use grapes::gio::ListStore;
+use grapes::glib::{self, GString, clone};
 use grapes::gtk::gdk::Key;
-use grapes::layer_shell::{Edge, KeyboardMode, Layer, LayerShell};
-use grapes::prelude::{GtkWindowExt, WidgetExt};
+use grapes::gtk::{
+    EventControllerKey, GridView, Label, ListItem, ListView,
+    SignalListItemFactory, SingleSelection, StringList, Widget,
+};
+use grapes::layer_shell::{KeyboardMode, Layer, LayerShell};
+use grapes::prelude::{
+    BoxExt, Cast, CastNone, GObjectPropertyExpressionExt, GtkWindowExt,
+    ListItemExt, WidgetExt,
+};
 use grapes::{
     WindowComponent,
     gtk::{self, ApplicationWindow},
 };
+use gtk::StringObject;
+
+enum View {
+    Grid(GridView),
+    List(ListView),
+}
 
 #[derive(WindowComponent)]
 pub struct Launcher {
     #[root]
     window: ApplicationWindow,
+
+    container: gtk::Box,
+    entry: gtk::Entry,
+    list_view: ListView,
 }
 
 impl Launcher {
     pub fn new(application: &gtk::Application) -> Self {
         let window = Self::create_configured_application_window(application);
 
-        Self { window }
+        let container = gtk::Box::new(gtk::Orientation::Vertical, 0);
+        window.set_child(Some(&container));
+
+        let entry = gtk::Entry::new();
+        container.append(&entry);
+
+        let list = gtk::ListBox::new();
+
+        for number in 0..=100 {
+            let label = gtk::Label::new(Some(&number.to_string()));
+            list.append(&label);
+        }
+
+        let scrollable = gtk::ScrolledWindow::builder()
+            .hscrollbar_policy(gtk::PolicyType::Never)
+            .min_content_width(360)
+            .child(&list)
+            .build();
+
+        container.append(&scrollable);
+
+        let model: StringList =
+            (0..=100_000).map(|number| number.to_string()).collect();
+
+        let selection_model = SingleSelection::new(Some(model));
+
+        let factory = SignalListItemFactory::new();
+        factory.connect_setup(move |_, list_item| {
+            let label = Label::new(None);
+            let list_item = list_item
+                .downcast_ref::<ListItem>()
+                .expect("Needs to be ListItem");
+            list_item.set_child(Some(&label));
+
+            list_item
+                .property_expression("item")
+                .chain_property::<StringObject>("string")
+                .bind(&label, "label", Widget::NONE);
+        });
+
+        let list_view = ListView::new(Some(selection_model), Some(factory));
+
+        container.append(&list_view);
+
+        Self {
+            window,
+            container,
+            entry,
+            list_view,
+        }
     }
 
     pub fn toggle_visibility(&self) {
@@ -35,14 +101,12 @@ impl Launcher {
     ) -> gtk::ApplicationWindow {
         let window = gtk::ApplicationWindow::new(application);
         window.init_layer_shell();
+        window.set_size_request(600, 400);
 
         window.set_widget_name("launcher");
 
-        let label = gtk::Label::new(Some("IT WORKS"));
-        window.set_child(Some(&label));
-
         window.set_namespace(Some("chameleon-launcher"));
-        // window.set_exclusive_zone(-1);
+        window.set_exclusive_zone(0);
         window.set_layer(Layer::Overlay);
         window.set_keyboard_mode(if true {
             KeyboardMode::Exclusive
@@ -50,10 +114,10 @@ impl Launcher {
             KeyboardMode::OnDemand
         });
 
-        window.set_anchor(Edge::Left, true);
-        window.set_anchor(Edge::Right, true);
-        window.set_anchor(Edge::Top, true);
-        window.set_anchor(Edge::Bottom, true);
+        // window.set_anchor(Edge::Left, true);
+        // window.set_anchor(Edge::Right, true);
+        // window.set_anchor(Edge::Top, true);
+        // window.set_anchor(Edge::Bottom, true);
 
         let controller = EventControllerKey::new();
         controller.connect_key_pressed(clone!(
@@ -62,10 +126,9 @@ impl Launcher {
             move |_, key, _, _| {
                 if key == Key::Escape {
                     window.set_visible(false);
-                    glib::Propagation::Stop
-                } else {
-                    glib::Propagation::Proceed
                 }
+
+                glib::Propagation::Stop
             }
         ));
         window.add_controller(controller);
