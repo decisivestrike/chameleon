03acc8353aa62198c3e66c084f5ae95bc55222b0
inqlog <not@example.com>
2026-03-03 18:44:55 +0300

    add sorting and filtering to launcher

--- chameleon-launcher/src/lib.rs
+++ chameleon-launcher/src/lib.rs
@@ -1,14 +1,14 @@
-use grapes::gio::ListStore;
-use grapes::glib::{self, GString, clone};
+use grapes::glib::{self, clone};
 use grapes::gtk::gdk::Key;
 use grapes::gtk::{
-    EventControllerKey, GridView, Label, ListItem, ListView, PolicyType,
-    ScrolledWindow, SignalListItemFactory, SingleSelection, StringList, Widget,
+    EventControllerKey, FilterListModel, GridView, Label, ListItem,
+    ListItemFactory, ListView, PolicyType, ScrolledWindow, SelectionModel,
+    SignalListItemFactory, SingleSelection, SortListModel, StringList, Widget,
 };
 use grapes::layer_shell::{KeyboardMode, Layer, LayerShell};
 use grapes::prelude::{
-    BoxExt, Cast, CastNone, GObjectPropertyExpressionExt, GtkWindowExt,
-    ListItemExt, WidgetExt,
+    BoxExt, Cast, CastNone, FilterExt, GObjectPropertyExpressionExt,
+    GtkWindowExt, IsA, ListItemExt, ListModelExt, WidgetExt,
 };
 use grapes::{
     WindowComponent,
@@ -38,30 +38,13 @@ impl Launcher {
         let entry = gtk::Entry::new();
         container.append(&entry);
 
-        let model: StringList =
-            (0..=100_000).map(|number| number.to_string()).collect();
+        let model = Self::create_model();
+        let factory = Self::create_factory();
 
-        let selection_model = SingleSelection::new(Some(model));
-
-        let factory = SignalListItemFactory::new();
-        factory.connect_setup(move |_, list_item| {
-            let label = Label::new(None);
-            let list_item = list_item
-                .downcast_ref::<ListItem>()
-                .expect("Needs to be ListItem");
-            list_item.set_child(Some(&label));
-
-            list_item
-                .property_expression("item")
-                .chain_property::<StringObject>("string")
-                .bind(&label, "label", Widget::NONE);
-        });
-
-        let list_view = ListView::new(Some(selection_model), Some(factory));
+        let list_view = ListView::new(Some(model), Some(factory));
 
         let scrolled_window = ScrolledWindow::builder()
             .hscrollbar_policy(PolicyType::Never)
-            .vscrollbar_policy(PolicyType::Automatic)
             .min_content_width(360)
             .min_content_height(400)
             .child(&list_view)
@@ -89,6 +72,58 @@ impl Launcher {
         // }
     }
 
+    fn create_factory() -> impl IsA<ListItemFactory> {
+        let factory = SignalListItemFactory::new();
+
+        factory.connect_setup(move |_, list_item| {
+            let label = Label::new(None);
+            let list_item = list_item
+                .downcast_ref::<ListItem>()
+                .expect("Needs to be ListItem");
+            list_item.set_child(Some(&label));
+
+            list_item
+                .property_expression("item")
+                .chain_property::<StringObject>("string")
+                .bind(&label, "label", Widget::NONE);
+        });
+
+        factory
+    }
+
+    fn create_model() -> impl IsA<SelectionModel> {
+        // test data
+        let model: StringList =
+            (0..=100_000).map(|number| number.to_string()).collect();
+
+        let filter = gtk::CustomFilter::new(move |obj| {
+            let string_object = obj
+                .downcast_ref::<StringObject>()
+                .expect("The object needs to be of type `IntegerObject`.");
+
+            string_object.string().contains('0')
+        });
+        let filter_model = FilterListModel::new(Some(model), Some(filter));
+
+        let sorter = gtk::CustomSorter::new(move |obj1, obj2| {
+            let string_object_1 = obj1
+                .downcast_ref::<StringObject>()
+                .expect("The object needs to be of type `StringObject`.");
+            let string_object_2 = obj2
+                .downcast_ref::<StringObject>()
+                .expect("The object needs to be of type `StringObject`.");
+
+            let str_1 = string_object_1.string();
+            let str_2 = string_object_2.string();
+
+            str_2.len().cmp(&str_1.len()).into()
+        });
+        let filter_and_sort_model =
+            SortListModel::new(Some(filter_model), Some(sorter.clone()));
+
+        SingleSelection::new(Some(filter_and_sort_model))
+    }
+
     fn create_configured_application_window(
         application: &gtk::Application,
     ) -> gtk::ApplicationWindow {
@@ -99,8 +134,8 @@ impl Launcher {
         window.set_widget_name("launcher");
 
         window.set_namespace(Some("chameleon-launcher"));
-        window.set_exclusive_zone(0);
-        window.set_layer(Layer::Overlay);
+        window.set_exclusive_zone(-1);
+        window.set_layer(Layer::Top);
         window.set_keyboard_mode(if true {
             KeyboardMode::Exclusive
         } else {
