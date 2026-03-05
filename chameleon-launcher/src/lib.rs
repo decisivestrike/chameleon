87c32a0c5acd6ea7f92e3a86986307680c225639
Denis <zak63507@gmail.com>
2026-03-05 16:21:23 +0300

    minor fixes

--- chameleon-launcher/src/lib.rs
+++ chameleon-launcher/src/lib.rs
@@ -52,8 +52,10 @@ impl Launcher {
 
         let scrolled_window = ScrolledWindow::builder()
             .hscrollbar_policy(PolicyType::Never)
+            .vscrollbar_policy(PolicyType::Automatic)
             .min_content_width(360)
             .min_content_height(400)
+            .overlay_scrolling(true)
             .child(&list_view)
             .build();
 
