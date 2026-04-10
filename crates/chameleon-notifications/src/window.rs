88dd0820a04b1d95fe953d9b509fc9a2c8a811f3
inqlog <not@example.com>
2026-04-10 17:00:01 +0300

    refactor(notifications): minor changes

--- crates/chameleon-notifications/src/window.rs
+++ crates/chameleon-notifications/src/window.rs
@@ -16,30 +16,11 @@ pub struct NotificationWindow {
 impl NotificationWindow {
     pub fn new(app: &gtk::Application, data: NotificationData) -> Self {
         let window = ApplicationWindow::new(app);
+        Self::setup_layershell(&window);
 
-        window.init_layer_shell();
-        window.set_namespace(Some("chameleon-notifications"));
-
-        window.set_anchor(Edge::Top, true);
-        window.set_margin(Edge::Top, GAP);
-
-        window.set_anchor(Edge::Right, true);
-        window.set_margin(Edge::Right, GAP);
-
-        let title = gtk::Label::new(Some(&data.title));
-        title.set_widget_name("title");
-
-        let body = gtk::Label::new(Some(&data.body));
-        body.set_widget_name("body");
-
-        let container = gtk::Box::new(Orientation::Vertical, 10);
-        container.append(&title);
-        container.append(&body);
-        container.set_widget_name("notification");
-
-        window.set_child(Some(&container));
+        let root = Self::build_ui(&data);
+        window.set_child(Some(&root));
         window.set_widget_name("notification-window");
-
         window.connect_destroy(move |window| {
             let ptr = window.as_ptr();
             glib::spawn_future_local(NOTIFICATION_WINDOWS.remove(ptr as usize));
@@ -56,6 +37,37 @@ impl NotificationWindow {
         });
 
         Self { window }
+    }
+
+    pub fn setup_layershell(window: &ApplicationWindow) {
+        window.init_layer_shell();
+        window.set_namespace(Some("chameleon-notifications"));
+
+        window.set_anchor(Edge::Top, true);
+        window.set_margin(Edge::Top, GAP);
+
+        window.set_anchor(Edge::Right, true);
+        window.set_margin(Edge::Right, GAP);
+    }
+
+    pub fn build_ui(data: &NotificationData) -> gtk::Box {
+        let title = gtk::Label::builder()
+            .label(&data.title)
+            .name("title")
+            .build();
+
+        let body = gtk::Label::builder().label(&data.body).name("body").build();
+
+        let container = gtk::Box::builder()
+            .orientation(Orientation::Vertical)
+            .spacing(10)
+            .name("notification")
+            .build();
+
+        container.append(&title);
+        container.append(&body);
+
+        container
     }
 
     pub fn as_ptr(&self) -> *mut GtkApplicationWindow {
