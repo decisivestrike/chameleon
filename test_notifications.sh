efcca4226caa65ce45bff3af51ce98d054306de4
inqlog <not@example.com>
2026-04-12 15:03:32 +0300

    chore(notifications): update test script

--- test_notifications.sh
+++ test_notifications.sh
@@ -2,14 +2,23 @@
 
 set -e
 
+# Group 1: No replace (10 separate notifications)
 for i in {1..10}; do
-    busctl --user call \
-        org.freedesktop.Notifications \
-        /org/freedesktop/Notifications \
-        org.freedesktop.Notifications \
-        Notify \
-        susssasa\{sv\}i \
-        "test" 0 "dialog-information" "Test $i" "Some body" 0 0 5000
-
+    notify-send --icon=dialog-information \
+        "No Replace $i" "Separate notification #$i" -t 2000
     sleep 0.3
 done
+
+sleep 1
+
+# Group 2: With replace (10 updates of one notification)
+ID=$(notify-send -p --icon=dialog-information \
+    "Replace Group" "First in replacement group" -t 12000)
+
+for i in {2..10}; do
+    notify-send -r "$ID" --icon=dialog-information \
+        "Replace Group $i" "Replacement #$i" -t 4000
+    sleep 0.3
+done
+
+echo "Done"
