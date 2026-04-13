30218077d1456ff414de730ff593cb6148c1e6dd
inqlog <not@example.com>
2026-04-13 19:20:16 +0300

    chore(notifications): add some doc comments

--- crates/chameleon-notifications/src/requests/notification_data/hints.rs
+++ crates/chameleon-notifications/src/requests/notification_data/hints.rs
@@ -24,22 +24,44 @@ pub struct NotificationHints {
     /// correct icon for the application, for logging purposes, etc.
     pub desktop_entry: Option<String>,
 
+    /// This is a raw data image format which describes the width, height,
+    /// rowstride, has alpha, bits per sample, channels and image data
+    /// respectively.
     pub image_data: Option<ImageData>,
 
+    /// Alternative way to define the notification image.
     pub image_path: Option<String>,
 
+    /// When set the server will not automatically remove the notification when
+    /// an action has been invoked. The notification will remain resident in
+    /// the server until it is explicitly removed by the user or by the sender.
+    /// This hint is likely only useful when the server has the "persistence"
+    /// capability.
     pub resident: Option<bool>,
 
+    /// The path to a sound file to play when the notification pops up.
     pub sound_file: Option<String>,
 
+    /// A themeable named sound from the freedesktop.org sound naming
+    /// specification to play when the notification pops up. Similar to
+    /// icon-name, only for sounds. An example would be "message-new-instant".
     pub sound_name: Option<String>,
 
+    /// Causes the server to suppress playing any sounds, if it has that
+    /// ability. This is usually set when the client itself is going to play
+    /// its own sound.
     pub suppress_sound: Option<bool>,
 
+    /// When set the server will treat the notification as transient and
+    /// by-pass the server's persistence capability, if it should exist.
     pub transient: Option<bool>,
 
+    /// Specifies the X location on the screen that the notification should
+    /// point to. The "y" hint must also be specified.
     pub x: Option<i32>,
 
+    /// Specifies the Y location on the screen that the notification should
+    /// point to. The "x" hint must also be specified.
     pub y: Option<i32>,
 
     /// The urgency level.
