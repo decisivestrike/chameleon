6d0c11efe2665e636b36c781c351779052881c1d
inqlog <not@example.com>
2026-04-16 18:22:29 +0300

    feat(core): add read_config function

--- crates/chameleon-core/src/lib.rs
+++ crates/chameleon-core/src/lib.rs
@@ -1,1 +1,39 @@
 pub mod errors;
+
+use serde::de::DeserializeOwned;
+use std::env::home_dir;
+use std::path::{Path, PathBuf};
+use std::sync::LazyLock;
+use std::{fmt, fs};
+
+pub static DEFAULT_CONFIG_FOLDER: LazyLock<PathBuf> = LazyLock::new(|| {
+    let maybe_home = home_dir();
+
+    if let Some(home) = maybe_home {
+        home.join(".config").join("chameleon")
+    } else {
+        panic!("Cant get $HOME")
+    }
+});
+
+pub fn read_config<Config, P>(config_path: P) -> Config
+where
+    Config: DeserializeOwned,
+    P: AsRef<Path> + fmt::Debug,
+{
+    let toml_str = match fs::read_to_string(&config_path) {
+        Ok(file) => file,
+        Err(e) => {
+            log::error!("Failed to open config file at {config_path:?}. {e}");
+            std::process::exit(-1);
+        }
+    };
+
+    match toml::from_str(&toml_str) {
+        Ok(config) => config,
+        Err(e) => {
+            log::error!("{e}");
+            std::process::exit(-1);
+        }
+    }
+}
