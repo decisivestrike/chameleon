bda1ae9e1b17f35db7823c27922d4c82cc8f63a9
inqlog <not@example.com>
2026-05-11 22:49:48 +0300

    feat(notifications): icon from app_icon

--- crates/chameleon-notifications/src/window/content.rs
+++ crates/chameleon-notifications/src/window/content.rs
@@ -1,15 +1,15 @@
 use crate::config::ContentRules;
 use crate::notification::{NotificationData, Urgency};
-use gtk::Orientation;
 use gtk::gdk::MemoryTexture;
 use gtk::prelude::BoxExt;
+use gtk::{Orientation, pango};
 use gtke::Component;
 
 #[derive(Clone, Component)]
 pub struct NotificationContent {
     pub icon: Option<gtk::Image>,
     pub title: gtk::Label,
-    pub body: gtk::Label,
+    pub body: Option<gtk::Label>,
     pub text_container: gtk::Box,
 
     #[root]
@@ -20,45 +20,82 @@ impl NotificationContent {
     const NAME: &str = "notification";
 
     pub fn new(data: &NotificationData, content_rules: &ContentRules) -> Self {
-        let title = gtk::Label::builder()
-            .label(&data.title)
-            .name("summary")
-            .build();
-        let body = gtk::Label::builder().label(&data.body).name("body").build();
-
         let urgency = data.hints.urgency.unwrap_or(Urgency::Low);
         let urgency_class = urgency.as_ref();
 
         let text_container = gtk::Box::builder()
             .orientation(Orientation::Vertical)
-            .spacing(10)
+            .halign(gtk::Align::Center)
+            .valign(gtk::Align::Center)
+            .hexpand(true)
+            .vexpand(true)
+            .baseline_position(gtk::BaselinePosition::Center)
+            .name("text-container")
             .build();
 
+        let title = gtk::Label::builder()
+            .label(&data.title)
+            .name("summary")
+            .build();
         text_container.append(&title);
-        text_container.append(&body);
+
+        let body = if !data.body.is_empty() {
+            let body = gtk::Label::builder()
+                .label(&data.body)
+                .name("body")
+                .wrap(true)
+                .wrap_mode(pango::WrapMode::Word)
+                .build();
+
+            text_container.append(&body);
+
+            Some(body)
+        } else {
+            None
+        };
 
         let container = gtk::Box::builder()
             .orientation(Orientation::Horizontal)
-            .spacing(10)
+            .halign(gtk::Align::Center)
+            .valign(gtk::Align::Center)
+            .baseline_position(gtk::BaselinePosition::Center)
             .name(Self::NAME)
             .css_classes([urgency_class])
             .build();
 
         container.append(&text_container);
 
-        let icon = if let Some(ref image_data) = data.hints.image_data {
-            let texture = MemoryTexture::from(image_data);
-            let icon = gtk::Image::builder()
-                .paintable(&texture)
-                .pixel_size(content_rules.icon_size.into())
-                .build();
+        let icon =
+            match (data.app_icon.as_str(), data.hints.image_data.as_ref()) {
+                ("", None) => None,
+                ("", Some(image_data)) => {
+                    let texture = MemoryTexture::from(image_data);
+                    let icon = gtk::Image::builder()
+                        .paintable(&texture)
+                        .halign(gtk::Align::Center)
+                        .valign(gtk::Align::Center)
+                        .name("icon")
+                        .pixel_size(content_rules.icon_size.into())
+                        .build();
 
-            container.prepend(&icon);
+                    container.prepend(&icon);
 
-            Some(icon)
-        } else {
-            None
-        };
+                    Some(icon)
+                }
+                (filename, _) => {
+                    let icon = gtk::Image::builder()
+                        .file(filename)
+                        .halign(gtk::Align::Center)
+                        .valign(gtk::Align::Center)
+                        .name("icon")
+                        .pixel_size(96)
+                        .build();
+
+                    container.prepend(&icon);
+
+                    Some(icon)
+                }
+            };
 
         Self {
             title,
@@ -68,18 +105,4 @@ impl NotificationContent {
             container,
         }
     }
-
-    // pub fn set_icon(&mut self, texture: MemoryTexture) {
-    //     if let Some(ref icon) = self.icon {
-    //         icon.set_paintable(Some(&texture));
-    //     } else {
-    //         let icon = gtk::Image::builder()
-    //             .paintable(&texture)
-    //             .pixel_size(32 /* CONFIG.icon_size.into() */)
-    //             .build();
-
-    //         self.container.prepend(&icon);
-    //         self.icon = Some(icon);
-    //     }
-    // }
 }
