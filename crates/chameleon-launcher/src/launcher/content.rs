6feca37db2c38e010cb5bfcba523b1b7f0c1d62f
inqlog <not@example.com>
2026-06-16 16:27:56 +0300

    launcher: separate the window and the content

---
+++ crates/chameleon-launcher/src/launcher/content.rs
@@ -0,0 +1,194 @@
+use crate::config::LauncherConfig;
+use crate::providers::Provider;
+use gtk::glib::subclass::types::ObjectSubclassIsExt;
+use gtk::glib::{Object, clone};
+use gtk::prelude::*;
+use gtk::{Align, PolicyType, ScrolledWindow, StackTransitionType, glib};
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::rc::Rc;
+use tracing::info;
+
+mod imp {
+    use super::*;
+    use crate::providers::Provider;
+    use gtk::prelude::OrientableExt;
+    use gtk::subclass::prelude::*;
+    use gtk::{Align, Orientation};
+
+    #[derive(Default)]
+    pub struct LauncherContentImp {
+        pub searchbar: gtk::Entry,
+        pub switcher: gtk::StackSwitcher,
+        pub stack: gtk::Stack,
+        pub count: gtk::Label,
+        pub providers: RefCell<HashMap<String, Rc<dyn Provider>>>,
+    }
+
+    #[glib::object_subclass]
+    impl ObjectSubclass for LauncherContentImp {
+        const NAME: &'static str = "ChameleonLauncherContent";
+        type Type = super::LauncherContent;
+        type ParentType = gtk::Box;
+    }
+
+    impl ObjectImpl for LauncherContentImp {
+        fn constructed(&self) {
+            self.parent_constructed();
+
+            self.stack.set_hhomogeneous(false);
+            self.stack.set_vhomogeneous(false);
+            self.stack.set_interpolate_size(false);
+            self.stack.set_transition_duration(0);
+            self.stack.set_transition_type(StackTransitionType::None);
+
+            self.switcher.set_stack(Some(&self.stack));
+            self.switcher.set_hexpand(false);
+            self.switcher.set_halign(Align::Center);
+
+            let box_ = self.obj();
+            box_.set_orientation(Orientation::Vertical);
+            box_.set_valign(Align::Fill);
+            box_.set_halign(Align::Fill);
+            box_.set_hexpand(true);
+            box_.set_vexpand(true);
+            box_.set_homogeneous(false);
+            box_.set_spacing(0);
+            box_.set_widget_name("launcher");
+
+            box_.append(&self.searchbar);
+            box_.append(&self.switcher);
+            box_.append(&self.stack);
+            box_.append(&self.count);
+        }
+    }
+
+    impl WidgetImpl for LauncherContentImp {}
+
+    impl BoxImpl for LauncherContentImp {}
+}
+
+glib::wrapper! {
+    pub struct LauncherContent(ObjectSubclass<imp::LauncherContentImp>)
+        @extends gtk::Box, gtk::Widget,
+        @implements gtk::Accessible, gtk::Buildable, gtk::ConstraintTarget, gtk::Orientable;
+}
+
+impl LauncherContent {
+    pub fn new(config: LauncherConfig) -> Self {
+        let content: Self = Object::builder().build();
+        let imp = content.imp();
+
+        imp.searchbar
+            .set_placeholder_text(Some(&config.searchbar_placeholder));
+
+        let providers = config.instantiate_providers();
+
+        fn capitalize_unicode(s: &str) -> String {
+            let mut chars = s.chars();
+            match chars.next() {
+                None => String::new(),
+                Some(first) => {
+                    first.to_uppercase().collect::<String>()
+                        + chars.as_str().to_lowercase().as_str()
+                }
+            }
+        }
+
+        for p in providers.into_iter().rev() {
+            let scrolled_window = ScrolledWindow::builder()
+                .propagate_natural_height(true)
+                .halign(Align::Fill)
+                .valign(Align::Fill)
+                .hscrollbar_policy(PolicyType::Never)
+                .vscrollbar_policy(PolicyType::Automatic)
+                .can_focus(false)
+                .can_target(true)
+                .min_content_width(480)
+                .max_content_width(1200)
+                .min_content_height(0)
+                .max_content_height(420)
+                .hexpand(false)
+                .vexpand(false)
+                .overlay_scrolling(true)
+                .name(p.name())
+                .child(&p.view())
+                .build();
+
+            content.imp().stack.add_titled(
+                &scrolled_window,
+                Some(p.name()),
+                &capitalize_unicode(p.name()),
+            );
+
+            content
+                .imp()
+                .providers
+                .borrow_mut()
+                .insert(p.name().to_string(), p);
+        }
+
+        imp.searchbar.connect_changed(clone!(
+            #[strong]
+            content,
+            move |searchbar| {
+                let query = searchbar.text();
+                content.update_model(&query);
+            }
+        ));
+
+        imp.stack.connect_visible_child_name_notify(clone!(
+            #[strong]
+            content,
+            move |stack| {
+                if let Some(name) = stack.visible_child_name() {
+                    info!("Active provider: {}", name);
+                }
+                content.update_model_from_query();
+            }
+        ));
+
+        content
+    }
+
+    pub fn update_model_from_query(&self) {
+        let searchbar = &self.imp().searchbar;
+        let query = searchbar.text();
+        self.update_model(&query);
+    }
+
+    pub fn invoke_provider_action(&self) -> bool {
+        if let Some(provider) = self.active_provider() {
+            provider.invoke_action()
+        } else {
+            false
+        }
+    }
+
+    fn update_model(&self, query: &str) {
+        if let Some(provider) = self.active_provider() {
+            info!("Update: {}, {}", provider.name(), query);
+            provider.update_model(&query);
+
+            if provider.len() > 0 {
+                let label = format!("{}", provider.len().to_string());
+                self.imp().count.set_label(&label);
+                self.imp().count.set_visible(true);
+            } else {
+                self.imp().count.set_visible(false);
+            }
+        }
+    }
+
+    pub fn reset_state(&self) {
+        self.imp().searchbar.set_text("");
+        self.active_provider().map(|p| p.reset_selection());
+    }
+
+    fn active_provider(&self) -> Option<Rc<dyn Provider>> {
+        let name = self.imp().stack.visible_child_name()?.to_string();
+        let providers = self.imp().providers.borrow();
+
+        Some(providers.get(&name)?.clone())
+    }
+}
---
+++ crates/chameleon-launcher/src/launcher/window.rs
@@ -0,0 +1,142 @@
+use crate::config::LauncherConfig;
+use crate::launcher::content::LauncherContent;
+use gtk::gdk::Key;
+use gtk::glib::subclass::types::ObjectSubclassIsExt;
+use gtk::glib::{Object, clone};
+use gtk::prelude::*;
+use gtk::{EventControllerKey, PropagationPhase, glib};
+use layer_shell::{KeyboardMode, Layer, LayerShell};
+
+mod imp {
+    use super::*;
+    use crate::launcher::content::LauncherContent;
+    use gtk::glib;
+    use gtk::subclass::prelude::*;
+    use std::sync::OnceLock;
+
+    #[derive(Default)]
+    pub struct LauncherImp {
+        pub(crate) root: OnceLock<LauncherContent>,
+    }
+
+    #[glib::object_subclass]
+    impl ObjectSubclass for LauncherImp {
+        const NAME: &'static str = "ChameleonLauncher";
+        type Type = super::Launcher;
+        type ParentType = gtk::Window;
+    }
+
+    impl ObjectImpl for LauncherImp {
+        fn constructed(&self) {
+            self.parent_constructed();
+
+            let window = self.obj();
+
+            window.set_focusable(true);
+            window.set_vexpand(false);
+            window.set_hexpand(false);
+            window.set_default_size(-1, -1); // Auto size
+            window.set_widget_name("launcher-window");
+
+            window.init_layer_shell();
+            window.set_namespace(Some("chameleon-launcher"));
+            window.set_layer(Layer::Top);
+            window.set_keyboard_mode(if false {
+                KeyboardMode::Exclusive
+            } else {
+                KeyboardMode::OnDemand
+            });
+        }
+    }
+
+    impl WidgetImpl for LauncherImp {}
+
+    impl WindowImpl for LauncherImp {}
+}
+
+glib::wrapper! {
+    pub struct Launcher(ObjectSubclass<imp::LauncherImp>)
+        @extends gtk::Window, gtk::Widget,
+        @implements gtk::Accessible, gtk::Buildable, gtk::ConstraintTarget, gtk::Native, gtk::Root, gtk::ShortcutManager;
+}
+
+impl Launcher {
+    pub fn new(config: LauncherConfig) -> Self {
+        let launcher: Self = Object::builder().build();
+        launcher.init(config);
+
+        let root_controller = EventControllerKey::new();
+        root_controller.connect_key_pressed(clone!(
+            #[strong]
+            launcher,
+            move |_, key, _, _| {
+                match key {
+                    Key::Left => glib::Propagation::Stop,
+                    // Key::Down => {
+                    //     if let Some(provider) = launcher.active_provider() {
+                    //         provider.select_below();
+                    //     }
+                    //     glib::Propagation::Stop
+                    // }
+                    Key::Tab => {
+                        // let container = &launcher.imp().container;
+                        // container.focus_child().map(|focused| {
+                        //     container.set_focus_child(focused.next_sibling())
+                        // });
+
+                        glib::Propagation::Stop
+                    }
+                    Key::Return => {
+                        launcher.invoke_action();
+                        glib::Propagation::Stop
+                    }
+                    Key::Escape => {
+                        launcher.toggle_visibility();
+                        glib::Propagation::Stop
+                    }
+                    _ => glib::Propagation::Proceed,
+                }
+            }
+        ));
+        root_controller.set_propagation_phase(PropagationPhase::Capture);
+        launcher.add_controller(root_controller);
+
+        launcher
+    }
+
+    fn init(&self, config: LauncherConfig) {
+        let content = LauncherContent::new(config);
+        self.set_child(Some(&content));
+        self.imp()
+            .root
+            .set(content)
+            .expect("can't be already initialized");
+    }
+
+    fn root(&self) -> &LauncherContent {
+        self.imp().root.get().expect("must be initialized")
+    }
+
+    fn invoke_action(&self) {
+        let invoked = self.root().invoke_provider_action();
+
+        if invoked {
+            self.toggle_visibility()
+        }
+    }
+
+    pub fn update_model(&self) {
+        self.root().update_model_from_query();
+    }
+
+    pub fn toggle_visibility(&self) {
+        let target_visibility = !self.get_visible();
+
+        if target_visibility {
+            self.present();
+        } else {
+            self.set_visible(target_visibility);
+            self.root().reset_state();
+        }
+    }
+}
